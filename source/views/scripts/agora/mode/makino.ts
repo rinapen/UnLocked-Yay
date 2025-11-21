04e69dda7b3f1074cbc424a86593dfb2f471f1b9
xbkv <one.two.o.two@icloud.com>
2025-11-21 18:17:57 +0900

    refactor: agoraManagerに統一 [makino]

--- source/views/scripts/agora/mode/makino.ts
+++ source/views/scripts/agora/mode/makino.ts
@@ -1,35 +1,21 @@
-import AgoraRTC, {
-  IAgoraRTCClient,
-  IBufferSourceAudioTrack,
-  IMicrophoneAudioTrack,
-} from "agora-rtc-sdk-ng";
+import { IAgoraRTCClient } from "agora-rtc-sdk-ng";
 import { RtmChannel } from "agora-rtm-sdk";
-import { playTrack, sendEmoji, sendMessage } from "../../utils/agoraActions";
+import { AgoraActionManager } from "../../utils/agoraActionManager";
 
-export default async function handleMakinoMode(bot_id, rtmChannel: RtmChannel, rtcClient: IAgoraRTCClient) {
-    await playTrack("/assets/audio/makino/first.m4a", true, 1000, rtcClient);
-
-    const emotes = ["ま", "き", "の", "様", "最", "強"];
-    // const text = "まきの様最強ww"
-    const text = "釈迦釈迦チキンとかいう障害者がボコられるw"
-    // const emotes = ["上", "野", "え", "い", "と", ]
+export default async function handleMakinoMode(rtcClient: IAgoraRTCClient, rtmChannel: RtmChannel, bot_id: string) {
+    const agoraActionManager = new AgoraActionManager(rtcClient, rtmChannel, bot_id);
+    const sounds = [
+        "/assets/audio/makino/kick.m4a",
+    ];
+    agoraActionManager.handleKickAndMuteSound(sounds);
+    
+    const text = "まきの様最強"
+    
     let emoteIndex = 0;
-
-    setInterval(() => sendMessage(bot_id, text, rtmChannel), 100);
-    setInterval(() => sendEmoji(emotes[emoteIndex++ % emotes.length], rtmChannel),100);
-    setInterval(() => rtmChannel.sendMessage({ text: `requestLiftAudioMute` }), 50);
-
-
-    rtmChannel.on("ChannelMessage", async (message, memberId, messageProps) => {
-        const msgText = message.text;
-        if (typeof msgText === "string") {
-        const sounds = [
-            "/assets/audio/makino/kick.m4a",
-        ];
-        const sound = sounds[Math.floor(Math.random() * sounds.length)];
-        if (msgText.startsWith("kick") || msgText.startsWith("muteAudio")) {
-            await playTrack(sound, false, 1000, rtcClient);
-        }
-    }
-    })
+    const emotes = ["ま", "き", "の", "様", "最", "強"];
+    
+    setInterval(() => agoraActionManager.sendMessage(text), 100);
+    setInterval(() => agoraActionManager.sendEmoji(emotes[emoteIndex++ % emotes.length]), 100);
+    setInterval(() => agoraActionManager.requestLiftAudioMute(), 50);
+    await agoraActionManager.playTrack("/assets/audio/makino/first.m4a", true);
 }
